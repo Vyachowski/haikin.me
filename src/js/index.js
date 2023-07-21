24c0f88da3f306f5e502b089ce86e704c9654e1f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-21 19:55:28 +0300

    feat: database connector

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import "./import/modules";
 import "./import/components";
+
+// Получение текущего URL-адреса страницы
+const currentURL = window.location.href;
+
+// Формирование URL-адреса для PHP-скрипта
+const phpScriptURL = currentURL.replace(
+  "index.html",
+  "backend/database_connector.php"
+);
+
+// Выполнение запроса к PHP-скрипту
+fetch(phpScriptURL)
+  .then((response) => response.json())
+  .then((data) => {
+    const totalRows = data.totalRows;
+    console.log(`Всего строк в базе данных: ${totalRows}`);
+  })
+  .catch((error) => console.error("Ошибка:", error));
