cfdbcc8ec5bb808f8e8b8fdc6ccb2963e122cf4b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-22 10:16:48 +0300

    bugfix: delete excess script

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,2 @@
 import "./import/modules";
 import "./import/components";
-
-// Функция для выполнения запроса к PHP-скрипту
-function fetchData() {
-  fetch("http://haikin.me/php/database_connector.php", {
-    method: "POST", // Метод запроса
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // Выводим ответ в консоль
-    })
-    .catch((error) => console.error("Ошибка:", error));
-}
-
-// Вызываем функцию для выполнения запроса
-fetchData();
