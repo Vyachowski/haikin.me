d9c9cf3ff0b3f21b7453ab62c767b31d234dd877
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-27 20:25:56 +0200

    feat: basic cards markup

--- dist/components/previews.php
+++ dist/components/previews.php
@@ -1,25 +1,4 @@
 <section class="preview-list">
-  <article class="preview-card">
-    <header class="preview-card-header">
-      <time datetime="2023-01-27 9:01">January 27, 2024</time>
-      <h2 class="preview-card-title">Hello!</h2>
-    </header>
-    <main class="preview-card-content">
-      <p class="preview-card-text">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-        specimen book. It has survived not only five centuries,
-        but also the leap into electronic typesetting, remaining essentially
-        unchanged. It was popularised in the 1960s with the release of Letraset
-        sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    </main>
-    <footer class="preview-card-footer">
-      <a class="preview-card-link button" href="#">Read more</a>
-    </footer>
-  </article>
   <article class="promo-card">
     <header class="preview-card-header">
       <time datetime="2023-01-27 9:01">January 27, 2024</time>
@@ -50,6 +29,27 @@
     <footer class="preview-card-footer">
       <a class="promo-card-link button button-cta" href="#">Read more</a>
       <a class="promo-card-link button button-cta-secondary" href="#">Read more</a>
+    </footer>
+  </article>
+  <article class="preview-card">
+    <header class="preview-card-header">
+      <time datetime="2023-01-27 9:01">January 27, 2024</time>
+      <h2 class="preview-card-title">Hello!</h2>
+    </header>
+    <main class="preview-card-content">
+      <p class="preview-card-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+        specimen book. It has survived not only five centuries,
+        but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </main>
+    <footer class="preview-card-footer">
+      <a class="preview-card-link button" href="#">Read more</a>
     </footer>
   </article>
 </section>
