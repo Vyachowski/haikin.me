450e90b4826c9bcebbab563468f339cf1dedcfae
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-27 18:33:19 +0200

    feat: add markup for article previews

--- dist/components/previews.php
+++ dist/components/previews.php
@@ -1,8 +1,55 @@
 <section class="preview-list">
-  <article class="preview">
-    <time datetime="2023-01-27 9:01">January 27, 2024</time>
-    <h1 class="preview-title">Hello!</h1>
-    <p class="preview-text">Text!</p>
-    <a class="preview-link" href="#">Read more</a>
+  <article class="preview-card">
+    <header class="preview-card-header">
+      <time datetime="2023-01-27 9:01">January 27, 2024</time>
+      <h2 class="preview-card-title">Hello!</h2>
+    </header>
+    <main class="preview-card-content">
+      <p class="preview-card-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+        specimen book. It has survived not only five centuries,
+        but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </main>
+    <footer class="preview-card-footer">
+      <a class="preview-card-link button" href="#">Read more</a>
+    </footer>
+  </article>
+  <article class="promo-card">
+    <header class="preview-card-header">
+      <time datetime="2023-01-27 9:01">January 27, 2024</time>
+      <picture>
+        <!-- Источник для больших экранов -->
+        <source media="(min-width: 1200px)" srcset="images/test.webp">
+
+        <!-- Источник для средних экранов -->
+        <source media="(min-width: 600px)" srcset="images/test.webp">
+
+        <!-- Источник для маленьких экранов или по умолчанию -->
+        <img src="images/test.webp" class="preview-card-image" width="300" height="200" alt="Example image">
+      </picture>
+    </header>
+    <main class="preview-card-content">
+      <h2 class="preview-card-title">Hello!</h2>
+      <p class="preview-card-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+        specimen book. It has survived not only five centuries,
+        but also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    </main>
+    <footer class="preview-card-footer">
+      <a class="promo-card-link button button-cta" href="#">Read more</a>
+      <a class="promo-card-link button button-cta-secondary" href="#">Read more</a>
+    </footer>
   </article>
 </section>
