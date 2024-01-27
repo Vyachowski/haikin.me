bd7106c50aafde737d55b9c27cb67d9eba9c5da6
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-27 20:35:15 +0200

    feat: small improvements for language select and image styles

--- dist/components/header.php
+++ dist/components/header.php
@@ -1,9 +1,9 @@
 <header class="page-header">
   <a class="logo" href="">Slava Haikin</a>
   <label>
-    <select>
-      <option value="english">En</option>
-      <option value="russian">Ру</option>
+    <select class="language-select" onchange="location = this.value;">
+      <option class="language-select-option" value="index.php">En</option>
+      <option class="language-select-option" value="/ru/index.php">Ру</option>
     </select>
   </label>
 </header>
