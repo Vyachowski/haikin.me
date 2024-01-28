b17e6d2146a2c9105388077ff45caa4772cfc6b3
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-28 21:59:41 +0200

    feat: add menu script

--- dist/components/scripts.php
+++ dist/components/scripts.php
@@ -1,3 +1,10 @@
 <script>
+  const menuButton = document.querySelector(".header__toggle");
+  const pageHeaderMenu = document.querySelector(".header");
+  const pageBody = document.querySelector("body");
 
+  menuButton.addEventListener("click", () => {
+    pageHeaderMenu.classList.toggle("header--open");
+    pageBody.classList.toggle("menu-opened");
+  });
 </script>
