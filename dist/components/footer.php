69820ad3952e59868de620f0bf3017fb43529c9e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-28 21:37:52 +0200

    feat: footer and header markup

--- dist/components/footer.php
+++ dist/components/footer.php
@@ -1,13 +1,21 @@
-<footer>
-  <menu class="footer-list">
-    <li class="footer-item">
-      <a class="footer-link" href="#">Github</a>
-    </li>
-    <li class="footer-item">
-      <a class="footer-link" href="#">Twitter</a>
-    </li>
-    <li class="footer-item">
-      <a class="footer-link" href="#">Linkedin</a>
-    </li>
-  </menu>
+<footer class="footer">
+  <div class="footer__wrapper">
+    <nav class="footer__navbar">
+      <ul class="footer__nav-list list list--horizontal">
+        <li class="footer__nav-item">
+          <a class="footer__nav-link footer__nav-link--active" href="#">Blog</a>
+        </li>
+        <li class="footer__nav-item">
+          <a class="footer__nav-link" href="#">Services</a>
+        </li>
+        <li class="footer__nav-item">
+          <a class="footer__nav-link" href="#">Projects</a>
+        </li>
+        <li class="footer__nav-item">
+          <a class="footer__nav-link" href="#">CV&nbsp;&&nbsp;Contacts</a>
+        </li>
+      </ul>
+    </nav>
+    <p class="footer__author">Built by Slava Haikin</p>
+  </div>
 </footer>
