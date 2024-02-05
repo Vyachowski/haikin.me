6d20e0f0cfd72f5ba20403e5e6bf2d5893262746
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-05 21:44:40 +0200

    Delete dist directory

--- dist/components/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="<?= $description ?>"/>
-  <meta name="author" content="Slava Haikin">
-  <title><?= $title ?> – <?= $name ?></title>
-  <link rel="stylesheet" href="style.css">
-</head>
-
